f68388a908277873844290174d8112e8752213e9
glogzi <116293555+Glogzi@users.noreply.github.com>
2024-10-25 09:31:36 +0200

    finished login form

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from account import views
-from bookmarks.urls import urlpatterns
-
-
 
 urlpatterns = [
-    path('login/', views=views.login_views, name='login')
+    path('login/', views.login_views, name='login')
 ]
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, HttpResponse
 from account.forms import LoginForm
+
 # Create your views here.
 def login_views(request):
     if request.method == 'POST':
-        pass
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Uwierzytelnienie zakończyło się sukcesem')
+                else:
+                    return HttpResponse('konto jest zablokowane')
+            else:
+                return HttpResponse('Nieprawidłowe dane uwierzytelniające')
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form' : form})
