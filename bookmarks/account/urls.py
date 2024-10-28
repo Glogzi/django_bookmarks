de0fb76ce386a862d780716467b4438425c8acbb
glogzi <116293555+Glogzi@users.noreply.github.com>
2024-10-28 15:07:38 +0100

    kolejna wesoła lekcja z karczewskim

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,5 +5,6 @@ from account import views
 urlpatterns = [
     # path('login/', views.login_views, name='login')
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard')
 ]
